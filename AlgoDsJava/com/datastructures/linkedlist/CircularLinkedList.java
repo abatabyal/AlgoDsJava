eca0b5f4f7d3bad56afba7394a967e1253236904
abatabyal@gmail.com <abatabyal@gmail.com>
2017-09-10 04:18:26 -0500

    more

---
+++ AlgoDsJava/com/datastructures/linkedlist/CircularLinkedList.java
@@ -0,0 +1,41 @@
+package com.datastructures.linkedlist;
+
+import javax.print.event.PrintJobAttributeListener;
+
+public class CircularLinkedList {
+	Node head;
+	Node tail;
+	static class Node {
+		int data;
+		Node next;
+		
+		Node (int d) {
+			this.data = d;
+		}
+	}
+	
+	public void printList() {
+		Node n = head;
+		while(n != tail) {
+			System.out.print(n.data+" ");
+			n = n.next;
+		}
+		System.out.print(n.data+" ");
+	}
+	
+	public static void main(String[] args) {
+		CircularLinkedList cll = new CircularLinkedList();
+		cll.head = new Node(1);
+		Node second = new Node(2);
+		Node third = new Node(3);
+		cll.tail = new Node(4);
+		
+		cll.head.next = second;
+		second.next = third;
+		third.next = cll.tail;
+		cll.tail.next = cll.head;
+		cll.printList();
+		
+	}
+
+}
